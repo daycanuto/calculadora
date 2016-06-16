129ed13194dcba4914d4a5f04703614e02b6ea4e
daycanuto <daycanuto@gmail.com>
2016-06-16 18:02:24 -0300

    2.0

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -85,6 +85,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -98,6 +99,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "2";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -111,6 +113,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "3";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -124,6 +127,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "4";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -137,6 +141,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "5";
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -164,6 +169,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "7";
             this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -177,6 +183,7 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "8";
             this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -190,6 +197,7 @@
             this.button10.TabIndex = 10;
             this.button10.Text = "9";
             this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -203,6 +211,7 @@
             this.button11.TabIndex = 11;
             this.button11.Text = "0";
             this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -216,6 +225,7 @@
             this.button12.TabIndex = 12;
             this.button12.Text = "=";
             this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -229,6 +239,7 @@
             this.button13.TabIndex = 13;
             this.button13.Text = "+";
             this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -242,6 +253,7 @@
             this.button14.TabIndex = 14;
             this.button14.Text = "-";
             this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -269,6 +281,7 @@
             this.button16.TabIndex = 16;
             this.button16.Text = "/";
             this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // Calculadora
             // 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,9 +12,21 @@ namespace Calculadora
 {
     public partial class Calculadora : Form
     {
+        int num1 = new int();
+        int num2 = new int();
+        string operacao = "";
+        int resultado = new int();
+       
+
         public Calculadora()
         {
             InitializeComponent();
+        }
+
+        private void Num1()
+        {
+            num1 = Convert.ToInt32(Resultado.Text);
+            Resultado.Text = "";
         }
 
         private void Resultado_TextChanged(object sender, EventArgs e)
@@ -24,17 +36,112 @@ namespace Calculadora
 
         private void button1_Click(object sender, EventArgs e)
         {
+            num1 = 0;
+            num2 = 0;
+            Resultado.Text = null;
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "1";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "2";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "3";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "4";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "5";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+            Resultado.Text += "6";
+        }
 
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "7";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "8";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Resultado.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            num2 = Convert.ToInt32(Resultado.Text);
+            switch (operacao)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    Resultado.Text = Convert.ToString(num1, "+" + num2, "= \n" + resultado);
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    Resultado.Text = Convert.ToString(num1, "+" + num2, "= \n" + resultado);
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    Resultado.Text = Convert.ToString(num1, "+" + num2, "= \n" + resultado);
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                        Resultado.Text = Convert.ToString(num1, "+" + num2, "= \n" + resultado);
+                    }
+                    else
+                    {
+                        Resultado.Text = ("Não é possível realizar esta divisão!");
+                    }
+                    break;
+            }
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "+";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "-";
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-
+            Resultado.Text = "*";
         }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "/";
+        }
+
     }
 }
